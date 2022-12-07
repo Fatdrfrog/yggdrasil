2ad8349647b8de50b8375325c0b2f098566eee7c
Samat <fatdrfrog@gmail.com>
2022-12-07 16:49:28 +0600

    starting tree

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="image">
+        <button className="button start">Start</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
