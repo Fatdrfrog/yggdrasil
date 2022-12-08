baa2c495f7b3133465503df80a966057d54fb431
Samat <fatdrfrog@gmail.com>
2022-12-08 13:02:13 +0600

    added few more tasks

--- src/App.jsx
+++ src/App.jsx
@@ -82,52 +82,87 @@ function App() {
               <div className="sub-tree" style={{ top: "30%" }}>
                 <button
                   className="button btn2"
-                  onClick={() =>
-                    handleSetConditions("isSecondButtonPressed", 2)
+                  onClick={() => {
+                    handleSetConditions("isSecondButtonPressed", 2);
+                    setCurrentTask(2);
+                  }}
+                  disabled={
+                    conditions.isSecondButtonPressed ||
+                    conditions.firstTaskResult !== 2
                   }
-                  disabled={conditions.isSecondButtonPressed}
                 >
                   Hunters eating something
                 </button>
                 <button
                   className="button btn3"
-                  onClick={() =>
-                    handleSetConditions("isSecondButtonPressed", 3)
+                  onClick={() => {
+                    handleSetConditions("isSecondButtonPressed", 3);
+                    setCurrentTask(3);
+                  }}
+                  disabled={
+                    conditions.isSecondButtonPressed ||
+                    conditions.firstTaskResult !== 3
                   }
-                  disabled={conditions.isSecondButtonPressed}
                 >
                   Girl with very long hairs
                 </button>
                 <button
                   className="button btn4"
-                  onClick={() =>
-                    handleSetConditions("isSecondButtonPressed", 4)
+                  onClick={() => {
+                    handleSetConditions("isSecondButtonPressed", 4);
+                    setCurrentTask(4);
+                  }}
+                  disabled={
+                    conditions.isSecondButtonPressed ||
+                    conditions.firstTaskResult !== 4
                   }
-                  disabled={conditions.isSecondButtonPressed}
                 >
                   Bai doing bai stuff
                 </button>
 
                 {conditions.secondTaskResult && (
                   <div className="sub-tree" style={{ top: "40%" }}>
-                    <button
-                      className="button btn5"
-                      onClick={() =>
-                        handleSetConditions("isThirdButtonPressed", 5)
-                      }
-                      disabled={conditions.isThirdButtonPressed}
-                    >
-                      An apple dropped...
-                    </button>
-                    <button
-                      className="button btn6"
-                      onClick={() =>
-                        handleSetConditions("isThirdButtonPressed", 6)
-                      }
-                      disabled={conditions.isThirdButtonPressed}
-                    >
-                      "Apple" noises
-                    </button>
+                    {(conditions.secondTaskResult === 2 ||
+                      conditions.secondTaskResult === 3) && (
+                      <button
+                        className="button btn5"
+                        onClick={() => {
+                          handleSetConditions("isThirdButtonPressed", 5);
+                          setCurrentTask(5);
+                        }}
+                        disabled={conditions.isThirdButtonPressed}
+                      >
+                        An apple dropped...
+                      </button>
+                    )}
+                    {(conditions.secondTaskResult === 33 ||
+                      conditions.secondTaskResult === 4) && (
+                      <button
+                        className="button btn6"
+                        onClick={() => {
+                          handleSetConditions("isThirdButtonPressed", 6);
+                          setCurrentTask(6);
+                        }}
+                        disabled={conditions.isThirdButtonPressed}
+                      >
+                        "Apple" noises
+                      </button>
+                    )}
+
+                    <div className="sub-tree last">
+                      <p>
+                        Hey... you finally awake. <br />
+                        You were trying to cross the root, right? Jumped right
+                        into that Bai's soldiers ambush, same as us, and that
+                        thief over there. Damn you Hunters...
+                        <br />
+                        What kind of apple are you talking about?
+                      </p>
+
+                      <button className="button" onClick={handleStartAgain}>
+                        Start again?
+                      </button>
+                    </div>
                   </div>
                 )}
               </div>
--- src/components/Dialog/Tasks/index.jsx
+++ src/components/Dialog/Tasks/index.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export const Task1 = ({ handleSetConditions, handleRemoveTask }) => {
   const handleFindAnswer = (event) => {
     const conditionValue =
@@ -20,7 +22,9 @@ export const Task1 = ({ handleSetConditions, handleRemoveTask }) => {
         className="button"
         style={{ top: "50%" }}
         onChange={handleFindAnswer}
+        placeholder="Select from below..."
       >
+        <option style={{ background: "white" }}>Select from below...</option>
         <option>Arrow noises</option>
         <option>Song</option>
         <option>laughing out loud</option>
@@ -29,33 +33,45 @@ export const Task1 = ({ handleSetConditions, handleRemoveTask }) => {
   );
 };
 
-export const Task2 = ({ handleSetConditions }) => {
+export const Task2 = ({ handleSetConditions, handleRemoveTask }) => {
+  // you should use secondTaskResult with value of 2 here
+
   const handleFindAnswer = (event) => {
-    if (event.target.value === "495") {
-      handleSetConditions("firstTaskResult", 1);
-    }
+    console.log("you should find if the typed input is equal to 495..");
   };
 
   return (
     <>
       <p>
-        Hunters eating 1kg of apples. One is going to drop from their pockets.
-        They see you and asks: what is a 1kg cost of an apples? If you will not
-        answer correctly they are going to make you an apple... ans: 495tg
+        Hunters eating 1kg of apples. One is going to drop from their pockets...
+        <br />
+        After a while they begin to notice you: shall you be our guest and help
+        us with some calculations here:
+        <br /> What is a 1kg cost of an apples? If you will not answer correctly
+        they are going to make you an apple... <br />
+        <br />
+        Ans: 495tg
       </p>
-      <input onChange={handleFindAnswer} />
+      <input
+        className="button"
+        style={{ top: "60%" }}
+        onChange={handleFindAnswer}
+      />
     </>
   );
 };
 
-export const Task3 = ({ handleSetConditions }) => {
+export const Task3 = ({ handleSetConditions, handleRemoveTask }) => {
+  //y ou should use secondTaskResult with value of 3 if left was picked, and 33 if right was picked
+
   return (
     <>
       You see one girl, she says that she is Kyz Zhibek, and because of too long
-      hairs she couldn't concentrate on some falling apples. May be you should
-      look to the left branch, she heard some sounds with "apple" involved on
-      them.. Or You might pick the right branch, cause, you know, why not.
-      Anyway, she is going to join you in finding this interesting magic apple
+      hairs she couldn't concentrate on some falling apples. <br /> Maybe you
+      should look to the left branch, she heard some sounds with "apple"
+      involved on them.. Or You might pick the right branch, cause, you know,
+      why not. Anyway, she is going to join you in finding this interesting
+      magic apple
       <div style={{ display: "flex", justifyContent: "space-around" }}>
         <button>Pick left</button> <button>Pick right</button>
       </div>
@@ -63,7 +79,8 @@ export const Task3 = ({ handleSetConditions }) => {
   );
 };
 
-export const Task4 = ({ handleSetConditions }) => {
+export const Task4 = ({ handleSetConditions, handleRemoveTask }) => {
+  // you should use secondTaskResult with value of 4 here
   return (
     <>
       <p>
@@ -76,20 +93,57 @@ export const Task4 = ({ handleSetConditions }) => {
   );
 };
 
-export const Task5 = ({ handleSetConditions }) => {
+export const Task5 = ({ handleSetConditions, handleRemoveTask }) => {
+  // you should use thirdTaskResult with value of 5 here
+  const [bag, setBag] = useState([
+    "simple apple",
+    "pickle",
+    "rock",
+    "skissors",
+    "notebook",
+    "tomato",
+    "branch",
+    "Lego Kids",
+  ]);
+
+  const handleFindAnswer = () => {
+    console.log(
+      "remove from an array the tomato, and show the result to console!"
+    );
+  };
+
   return (
     <>
       <p>
-        Hunters eating 1kg of apples. One is going to drop from their pockets.
-        They see you and asks: what is a 1kg cost of an apples? If you will not
-        answer correctly they are going to make you an apple... ans: 495tg
+        Hunters shocked, an apple dropped. You see one girl looking for
+        something. Hunters running after you, you scream for a help..
+        <br /> Kyz Zhibek says, Find <b style={{ fontSize: "20px" }}>
+          tomato
+        </b>{" "}
+        from my bag with those items:
+        <br /> (
+        {bag.map((el) => (
+          <>{el + ", "}</>
+        ))}
+        ) <br />
+        and pretend it to be an apple. then drop to them, and run, when they
+        will chase the rock instead. <br />
+        task: [remove from an array the tomato]
       </p>
-      <input onChange={handleFindAnswer} />
+      <button
+        className="button"
+        style={{ bottom: "5%" }}
+        onClick={handleFindAnswer}
+      >
+        Find Tomato
+      </button>
     </>
   );
 };
 
-export const Task6 = ({ handleSetConditions }) => {
+export const Task6 = ({ handleSetConditions, handleRemoveTask }) => {
+  // you should use thirdTaskResult with value of 6 here
+
   return (
     <>
       <p>
